2a39b6943384035dc9fe498da2ff828ebd07227a
Gökalp Avcu <gokalp.avcu@santsg.com>
2022-07-29 13:29:41 +0300

    add changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
   const ref = useRef();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const enable = username && password; // username ve password boş değilse enable true olsun (username ve password girilmişse)
 
   useEffect(() => {
     let images = ref.current.querySelectorAll("img"),
@@ -57,8 +61,8 @@ function App() {
           />
         </div>
       </div>
-      <div className="w-[350px] bg-white border px-[50px] pt-8 pb-2">
-        <a href="#" className="flex justify-center">
+      <div className="w-[350px] bg-white border px-[40px] pt-10 pb-2">
+        <a href="#" className="flex justify-center mb-8">
           <img
             className="h-[51px]"
             src="https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png"
@@ -70,6 +74,8 @@ function App() {
           <label className="block relative">
             <input
               type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               required={true}
               className="bg-zinc-50 px-2 border rounded-sm outline-none text-xs focus:border-gray-400 w-full h-[38px] valid:pt-[10px] peer"
             />
@@ -80,6 +86,8 @@ function App() {
           <label className="block relative">
             <input
               type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               required={true}
               className="bg-zinc-50 px-2 border rounded-sm outline-none text-xs focus:border-gray-400 w-full h-[38px] valid:pt-[10px] peer"
             />
@@ -87,6 +95,21 @@ function App() {
               Password
             </small>
           </label>
+          <button
+            type="submit"
+            disabled={!enable} // enable değilse disabled olsun
+            className="h-[30px] rounded bg-brand font-semibold text-white text-sm disabled:opacity-50"
+          >
+            {" "}
+            Log In
+          </button>
+          <div className="flex items-center">
+            <div className="h-px bg-gray-300 flex-1" />
+            <span className="px-4 text-[13px] text-gray-500 font-semibold">
+              OR
+            </span>
+            <div className="h-px bg-gray-300 flex-1" />
+          </div>
         </form>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        brand: "#0095f6",
+      },
       backgroundImage: {
-        "logo-pattern": "url(https://www.instagram.com/static/images/homepage/phones/home-phones.png/1dc085cdb87d.png)"
-      }
+        "logo-pattern":
+          "url(https://www.instagram.com/static/images/homepage/phones/home-phones.png/1dc085cdb87d.png)",
+      },
     },
   },
   plugins: [],
-}
+};
