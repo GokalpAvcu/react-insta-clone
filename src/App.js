fd0a04f7f793b49b10d873e6c968dbcf00e29894
Gökalp Avcu <gokalp.avcu@santsg.com>
2022-07-29 11:01:43 +0300

    add changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from "react";
 
 function App() {
+  const ref = useRef();
+
+  useEffect(() => {
+    let images = ref.current.querySelectorAll("img"),
+      total = images.length,
+      current = 0;
+
+    const imageSlider = () => {
+      if (current > 0) {
+        images[current - 1].classList.add("opacity-0");
+      } else {
+        images[total - 1].classList.add("opacity-0");
+      }
+      images[current].classList.remove("opacity-0");
+      if (current === total - 1) {
+        current = 0;
+      } else {
+        current += 1;
+      }
+    };
+    imageSlider();
+    let interval = setInterval(imageSlider, 3000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [ref]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="h-full w-full flex flex-wrap overflow-auto items-center gap-x-8 justify-center">
+      <div className="hidden md:block w-[380.32px] h-[581.15px] bg-logo-pattern relative bg-[length:468.32px_634.15px] bg-[top_left_-46px]">
+        <div
+          className="w-[250px] h-[538px] absolute top-[27px] right-[18px]"
+          ref={ref}
         >
-          Learn React
+          <img
+            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear"
+            src="https://www.instagram.com/static/images/homepage/screenshots/screenshot1.png/fdfe239b7c9f.png"
+            alt=""
+          />
+          <img
+            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear"
+            src=" https://www.instagram.com/static/images/homepage/screenshots/screenshot2.png/4d62acb667fb.png"
+            alt=""
+          />
+          <img
+            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear"
+            src="https://www.instagram.com/static/images/homepage/screenshots/screenshot3.png/94edb770accf.png"
+            alt=""
+          />
+          <img
+            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear"
+            src="https://www.instagram.com/static/images/homepage/screenshots/screenshot4.png/a4fd825e3d49.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="w-[350px] bg-white border px-[50px] pt-8 pb-2">
+        <a href="#" className="flex justify-center">
+          <img
+            className="h-[51px]"
+            src="https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png"
+            alt=""
+          />
         </a>
-      </header>
+
+        <form className="grid gap-y-1.5">
+          <label className="block relative">
+            <input
+              type="text"
+              required={true}
+              className="bg-zinc-50 px-2 border rounded-sm outline-none text-xs focus:border-gray-400 w-full h-[38px] valid:pt-[10px] peer"
+            />
+            <small className="absolute top-1/2 left-[9px] cursor-text pointer-events-none text-xs text-gray-400 -translate-y-1/2  transition-all peer-valid:text-[10px] peer-valid:top-2.5 ">
+              Phone number, username or email
+            </small>
+          </label>
+          <label className="block relative">
+            <input
+              type="password"
+              required={true}
+              className="bg-zinc-50 px-2 border rounded-sm outline-none text-xs focus:border-gray-400 w-full h-[38px] valid:pt-[10px] peer"
+            />
+            <small className="absolute top-1/2 left-[9px] cursor-text pointer-events-none text-xs text-gray-400 -translate-y-1/2  transition-all peer-valid:text-[10px] peer-valid:top-2.5 ">
+              Password
+            </small>
+          </label>
+        </form>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "logo-pattern": "url(https://www.instagram.com/static/images/homepage/phones/home-phones.png/1dc085cdb87d.png)"
+      }
+    },
+  },
+  plugins: [],
+}
